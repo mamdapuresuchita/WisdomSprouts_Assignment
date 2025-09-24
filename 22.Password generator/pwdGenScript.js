67b3fd64b1d75ab251faf7f922cac7effaecfc12
Suchita <mamdapuresuchita@gmail.com>
2025-09-24 17:18:21 +0530

    added while loop
    Password was exceeding the length so used while loop

--- 22.Password generator/pwdGenScript.js
+++ 22.Password generator/pwdGenScript.js
@@ -5,9 +5,11 @@ symbolSet ="[]\;',./{}:<>?!@#$%^&*()_+-=";
 
 
 //selectors
+displayBox = document.querySelector("#result");
 
-displayBox = document.querySelector("#result")
-totalCharInput =document.querySelector("#lenOfPwd")
+
+
+totalChar =document.querySelector("#lenOfPwd")
 numInput = document.querySelector("#includeNum")
 upperInput = document.querySelector("#includeUpperCase")
 lowerInput = document.querySelector("#includeLowerCase")
@@ -23,7 +25,32 @@ let getRandomData =(dataSet)=>{
     return dataSet[Math.floor(Math.random()* dataSet.length)]
 }
 
+//while loop keep adding characters until the password length reaches the number entered.
 
-let generatePwd =(pwd = "")=>{
+const generatePassword = () => {
+      let password = "";
 
-}
+      while (password.length < totalChar.value) {
+        if (upperInput.checked && password.length < totalChar.value) {
+          password += getRandomData(upperCaseSet);
+        }
+        if (lowerInput.checked && password.length < totalChar.value) {
+          password += getRandomData(lowerCaseSet);
+        }
+        if (numInput.checked && password.length < totalChar.value) {
+          password += getRandomData(numSet);
+        }
+        if (splCharInput.checked && password.length < totalChar.value) {
+          password += getRandomData(symbolSet);
+        }
+      }
+
+      displayBox.value = password;
+      console.log(password);
+    };
+
+    document.getElementById("btn").addEventListener("click", generatePassword);
+
+    // Generate one on page load
+    generatePassword();
+
